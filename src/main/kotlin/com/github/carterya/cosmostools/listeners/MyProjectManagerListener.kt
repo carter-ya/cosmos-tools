9a445849f5aef1e37d32d37d67be094c8c1ebeb3
GitHub Action <action@github.com>
2022-07-02 08:10:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/carterya/cosmostools/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/carterya/cosmostools/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.carterya.cosmostools.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.carterya.cosmostools.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/carterya/cosmostools/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/carterya/cosmostools/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.carterya.cosmostools.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.carterya.cosmostools.MyBundle
 
 class MyApplicationService {
 
